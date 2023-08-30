4e1ad09184fd2c58e2117fc2da50c588eb5606d2
kucp23cc013 <141497956+kucp23cc013@users.noreply.github.com>
2023-08-30 11:59:45 +0000

    added env for database uri

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 // Create a database connection
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://mongo:27017/employees");
+    await mongoose.connect(process.env.MONGO_DB_URI);
     console.log("Database connected successfully");
   } catch (error) {
     console.error(error);
